c54bc9cd8410f3eeb2bdd3d43efb7a098ebcb082
jayaraj kannan <jayaraj.kannan@rntbci.com>
2024-08-06 22:02:14 +0530

    updated code

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,10 +1,8 @@
 <template>
   <ProfileHeader/>
-  <project/>
 </template>
 
 <script lang="ts" setup>
 import ProfileHeader from '@/components/ProfileHeader.vue';
-import project from '@/components/project.vue';
   //
 </script>
